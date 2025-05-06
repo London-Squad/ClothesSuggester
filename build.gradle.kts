29e530f5e4fd36b95d8a6b6095072d3ca452b4df
Dilman Arif <dilmanarif01@gmail.com>
2025-05-06 10:41:22 +0300

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "org.example"
@@ -11,6 +12,22 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(project.dependencies.platform("io.insert-koin:koin-bom:4.0.3"))
+    implementation("io.insert-koin:koin-core")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.0-M3")
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("io.mockk:mockk:1.14.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+
+    implementation("io.ktor:ktor-client-core:3.1.2")
+    implementation("io.ktor:ktor-client-cio:3.1.2")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
 }
 
 tasks.test {
