3cb0c321e4851688fe1ace985cfa267c4e9a640e
samarGamal24 <samar24.g@gmail.com>
2025-05-06 12:18:56 +0300

    make an interface for geocding

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-json:2.3.4")
+    implementation("io.ktor:ktor-client-serialization:2.3.4")
+    implementation("org.json:json:20231013")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.0")
 }
 
 tasks.test {
