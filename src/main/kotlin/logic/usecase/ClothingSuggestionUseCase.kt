038152906cb1aba1d9a99712c1b4783989080b7c
Mohammed-qmr <mma.mqamar@gmail.com>
2025-05-09 13:29:52 +0300

    git commit -m "Resolved merge conflict in ClothingSuggestionUseCase.kt"

--- src/main/kotlin/logic/usecase/ClothingSuggestionUseCase.kt
+++ src/main/kotlin/logic/usecase/ClothingSuggestionUseCase.kt
@@ -1,13 +1,33 @@
 package logic.usecase
 
+import data.geocoding.GeocodingService
 import logic.entity.ClothingType
 import logic.repository.WeatherRepository
 import logic.result.LogicResponse
 
+data class ClothingSuggestionResult(
+    val type: ClothingType,
+
+)
 class ClothingSuggestionUseCase(
-    private val weatherRepository: WeatherRepository
+    private val weatherRepository: WeatherRepository,
+
+
 ) {
-    operator fun invoke(countryName: String): LogicResponse<ClothingType> {
+    operator  fun invoke(countryName: String): LogicResponse<ClothingType> {
+
         TODO("wait for geoCoding")
     }
-}
+
+    private fun suggestClothing(temperature: Double, isRaining: Boolean): ClothingSuggestionResult {
+        val type = when {
+            temperature >= 25 -> ClothingType.LIGHT
+            temperature > 15 && temperature < 25 -> ClothingType.MEDIUM
+            else -> ClothingType.HEAVY
+        }
+
+        return ClothingSuggestionResult(type)
+    }
+    }
+
+
